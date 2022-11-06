a4b24c9bda9f584631128ed6b08893a81ccd71d4
erenst <erenst2002@hotmail.com>
2022-11-06 11:03:19 +1030

    Complete header component.

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,35 @@
 // @ts-nocheck
+import Image from "next/legacy/image";
+import { MagnifyingGlassIcon,Bars3Icon,UserCircleIcon,GlobeAltIcon } from "@heroicons/react/24/solid";
 function Header() {
     return (
-        <div>
-            <div>
-                I am the Header.
-            </div>
-
+        <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10 md:shadow-sm">
             {/*left */}
-            <div></div>
-            {/*middle */}
-            <div></div>
+            <div className="relative flex items-center h-10 cursor-pointer">
+                <Image
+                    src="https://links.papareact.com/qd3"
+                    layout="fill" objectFit="contain" objectPosition="left"
+
+                />
+            </div>
+            {/*middle - search */}
+            <div className="flex items-center md:border-2 rounded-full py-2">
+                <input type="text" placeholder="Start your search"
+                    className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400"
+                />
+                <MagnifyingGlassIcon className="hidden md:inline-flex h-8 
+                    bg-red-400 text-white rounded-full p-1 cursor-pointer md:mx-2" />
+            </div>
             {/*right */}
-            <div></div>
-        </div>
+            <div className="flex items-center space-x-4 justify-end text-gray-500">
+                <p className="hidden md:inline cursor-pointer">Become a host</p>
+                <GlobeAltIcon className="hidden md:inline h-6 cursor-pointer" />
+                <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+                    <Bars3Icon className="h-6 cursor-pointer" />
+                    <UserCircleIcon className="h-6 cursor-pointer" />
+                </div>
+            </div>
+        </header>
 
     );
 }
