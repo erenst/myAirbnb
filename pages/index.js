5cf643f6297368df726dea9c7aaa18eb74c307fc
erenst <erenst2002@hotmail.com>
2022-11-06 16:54:51 +1030

    Complete Large Card component.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@
 import Head from "next/head";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import LargeCard from "../components/LargeCard";
 import MediumCard from "../components/MediumCard";
 import SmallCard from "../components/SmallCard";
 
@@ -37,6 +38,13 @@ export default function Home({ exploreData,cardData }) {
             }
           </div>
         </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb."
+          buttonText="Get Inspired"
+        />
       </main>
     </div>
   );
